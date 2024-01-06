dfde7766a200c0d4bc759d0d67f9ede153c951a6
Dennis <dennisdes753@gmail.com>
2024-01-06 23:50:58 +0530

    changes push in mobile

--- src/CommonComponent/Header/header.jsx
+++ src/CommonComponent/Header/header.jsx
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Navbar, Container, Image } from 'react-bootstrap';
+import {NavLink} from 'react-router-dom';
 import {Logo} from '../../asset/img';
 import './header.scss';
 
@@ -11,6 +12,9 @@ function Header() {
                     <Navbar expand="lg" className="bg-body-tertiary">
                         <Navbar.Brand href="#home">
                             <Image className="img-fluid" src={Logo} alt="ToponFirLogo"/>
+                            <div className="our_app_button dsiplay-none">
+                                <NavLink to="https://apps.apple.com/in/app/toneop-health-and-fitness-app/id1586794292" target="_blank">Our App</NavLink>
+                            </div>
                         </Navbar.Brand>
                     </Navbar>
                 </Container>
