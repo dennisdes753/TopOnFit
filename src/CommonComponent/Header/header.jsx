744d9e4e68e640821189415f04dace95739f2c84
Dennis <dennisdes753@gmail.com>
2024-01-06 23:54:00 +0530

    minor changes in web

--- src/CommonComponent/Header/header.jsx
+++ src/CommonComponent/Header/header.jsx
@@ -12,7 +12,7 @@ function Header() {
                     <Navbar expand="lg" className="bg-body-tertiary">
                         <Navbar.Brand href="#home">
                             <Image className="img-fluid" src={Logo} alt="ToponFirLogo"/>
-                            <div className="our_app_button dsiplay-none">
+                            <div className="our_app_button display-none">
                                 <NavLink to="https://apps.apple.com/in/app/toneop-health-and-fitness-app/id1586794292" target="_blank">Our App</NavLink>
                             </div>
                         </Navbar.Brand>
